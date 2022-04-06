8bca8ffb80ccaf47d1a6f615f267b7e69abc6313
Quang <godyudodis@gmail.com>
2022-04-06 07:10:18 +0700

    Changing font to display Vietnamese

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Nerd Font :pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "Fira Code Nerd Font :pixelsize=15:antialias=true:autohint=true" };
+static char *font = "SauceCodePro Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { "SauceCodePro Nerd Font:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
